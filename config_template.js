654c77a652d9f24f79837a42386a802993db060d
h0uz3 <h0uz3@foto23.com>
2016-09-04 14:28:41 +0200

    Added config-template.

---
+++ config_template.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  owncloud: {
+    username: 'user@hostname.tld',
+    password: 'yourOwnCloudPassword',
+    server: 'EXAMPLE: https://HOSTNAME/remote.php/carddav/principals/user@hostname.tld'
+  },
+  targetmailbox: 'INBOX.lowprio',
+  sievefile: '/var/mail/sieve/lowprio.sieve'
+};
